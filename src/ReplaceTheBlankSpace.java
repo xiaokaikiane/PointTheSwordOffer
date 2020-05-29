bdf231b886f72088f003e115a604fad28718328b
xiaokaikiane <1131697410@qq.com>
2020-05-29 12:53:17 +0800

    剑指

---
+++ src/ReplaceTheBlankSpace.java
@@ -0,0 +1,13 @@
+public class ReplaceTheBlankSpace {
+    public String replaceSpace(StringBuffer str) {
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                str.replace(i,i+1,"%20");
+            }
+        }
+        return str.toString();
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/TwoDimensionalArrayLookUp.java
@@ -0,0 +1,16 @@
+public class TwoDimensionalArrayLookUp {//二位数组查找
+    public static boolean Find(int target, int[][] array) {
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                if(array[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Find(3,arr));
+    }
+}
