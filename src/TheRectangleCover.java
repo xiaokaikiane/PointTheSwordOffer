33558bcb3b3b559173b36d3a69ab67a4b7201b5b
xiaokaikiane <1131697410@qq.com>
2020-06-03 18:44:36 +0800

    剑指

---
+++ src/TheRectangleCover.java
@@ -0,0 +1,18 @@
+public class TheRectangleCover {
+    public int RectCover(int target) {
+        if(target<=0){
+            return 0;
+        }
+        if(target==1){
+            return 1;
+        }
+        if(target==2){
+            return 2;
+        }
+        return RectCover(target-1)+RectCover(target-2);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
