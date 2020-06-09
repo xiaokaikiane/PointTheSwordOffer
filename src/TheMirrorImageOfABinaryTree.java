036c221aa3c2acca7dfa51c221ea01f27a448133
xiaokaikiane <1131697410@qq.com>
2020-06-09 23:57:41 +0800

    剑指

---
+++ src/TheMirrorImageOfABinaryTree.java
@@ -0,0 +1,24 @@
+//二叉树的镜像
+public class TheMirrorImageOfABinaryTree {
+    static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    public void Mirror(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
