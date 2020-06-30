3adca5e583353e77710d37eb88dead8746dc9e8d
xiaokaikiane <1131697410@qq.com>
2020-06-30 23:01:02 +0800

    剑指

--- src/TheFirstCharacterThatAppearsOnlyOnce.java
+++ src/TheFirstCharacterThatAppearsOnlyOnce.java
@@ -6,29 +6,29 @@ import java.util.Map;
 // 出现一次的字符,并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
 // （从0开始计数）
 public class TheFirstCharacterThatAppearsOnlyOnce {
-    public int FirstNotRepeatingChar(String str) {
-        Map<Character,Integer> map=new HashMap<>();
-        Map<Character,Integer> map1=new HashMap<>();
-        char[] arr=str.toCharArray();
-        for(int i=0;i<arr.length;i++){
-            if(!map.containsKey(arr[i])){
-
-                map1.put(arr[i],map1.getOrDefault(arr[i],0)+i);
-            }
-            int n=map.getOrDefault(arr[i],0);
-            map.put(arr[i],n+1);
-        }
-        char k=0;
-        for(char key:map.keySet()){
-            if(map.get(key)==1){
-                k=key;
-            }
-        }
-        for (char key:map1.keySet()){
-            if(k==key){
-                return map1.get(key);
-            }
-        }
-        return -1;
-    }
+//    public int FirstNotRepeatingChar(String str) {
+//        Map<Character,Integer> map=new HashMap<>();
+//        Map<Character,Integer> map1=new HashMap<>();
+//        char[] arr=str.toCharArray();
+//        for(int i=0;i<arr.length;i++){
+//            if(!map.containsKey(arr[i])){
+//
+//                map1.put(arr[i],map1.getOrDefault(arr[i],0)+i);
+//            }
+//            int n=map.getOrDefault(arr[i],0);
+//            map.put(arr[i],n+1);
+//        }
+//        char k=0;
+//        for(char key:map.keySet()){
+//            if(map.get(key)==1){
+//                k=key;
+//            }
+//        }
+//        for (char key:map1.keySet()){
+//            if(k==key){
+//                return map1.get(key);
+//            }
+//        }
+//        return -1;
+//    }
 }
